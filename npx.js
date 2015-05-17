815bdf7043c4873a17b684ffdfb49323d06da9aa
Jasper St. Pierre <jstpierre@mecheye.net>
2015-05-17 11:21:53 -0700

    turn on depth for the ray-cast
    jewel wanted to see this

--- npx.js
+++ npx.js
@@ -411,7 +411,6 @@
             gl.enable(gl.DEPTH_TEST);
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
             models.forEach(renderModel);
-            gl.clear(gl.DEPTH_BUFFER_BIT);
             renderModel(rayCastModel);
         }
 
